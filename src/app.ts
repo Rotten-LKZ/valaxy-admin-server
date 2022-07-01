3802e91574bf32329d4a92855c24de6939cde9a5
Rotten-LKZ <Rotten_LKZ@163.com>
2022-07-02 00:47:50 +0800

    feat: redirect

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import jwt from './utils/jwt'
 import urlencode from 'urlencode'
 import result from './utils/result'
 import { createReadStream } from 'fs'
+import { createProxyMiddleware } from 'http-proxy-middleware'
 
 
 ;(async () => {
@@ -31,30 +32,34 @@ import { createReadStream } from 'fs'
   }))
   app.use(express.json())
   // Logger & Static images access
-  app.use((req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`)
-    const originalUrl = urlencode.decode(req.originalUrl, 'gbk')
-    if (originalUrl.endsWith('.jpeg') || originalUrl.endsWith('.jpg') || originalUrl.endsWith('.png')) {
-      try {
-        res.setHeader('Cache-Control', 'public, max-age=31536000')
-        const cs = createReadStream(path.resolve(originalUrl.replace(/v[0-9]+/, 'upload').substring(1)))
-        cs.on('data', (chunk) => {
-          res.write(chunk)
-        })
-        
-        cs.on('close', () => {
-          res.status(200)
+  if (process.env.REDIRECT_URL) {
+    app.use('/v1/[0-9]{4}/[0-9]{2}/[0-9]{2}/', createProxyMiddleware({ target: process.env.REDIRECT_URL, changeOrigin: false }))
+  } else {
+    app.use((req, res, next) => {
+      console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`)
+      const originalUrl = urlencode.decode(req.originalUrl, 'gbk')
+      if (/(\.jpeg|\.jpg|\.png)$/.test(originalUrl)) {
+        try {
+          res.setHeader('Cache-Control', 'public, max-age=31536000')
+          const cs = createReadStream(path.resolve(originalUrl.replace(/v[0-9]+/, 'upload').substring(1)))
+          cs.on('data', (chunk) => {
+            res.write(chunk)
+          })
+          
+          cs.on('close', () => {
+            res.status(200)
+            res.end()
+          })
+        }catch(e) {
+          console.error(e)
+          res.status(404)
           res.end()
-        })
-      }catch(e) {
-        console.error(e)
-        res.status(404)
-        res.end()
+        }
+      } else {
+        next()
       }
-    } else {
-      next()
-    }
-  })
+    })
+  }
   // Verify JWT
   app.use(async (req, res, next) => {
     if (!req.url.includes('/user/login') && !req.url.includes('/info') && !(await jwt.verify(req.headers.authorization?.split(' ')[1] || ''))) 
